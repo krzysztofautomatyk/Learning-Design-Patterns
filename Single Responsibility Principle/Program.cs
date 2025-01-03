bce82dadfbf6a87414a852de253b5f0622dbd131
Krzysztof Wozniczak <kwoznicz@volvocars.com>
2025-01-03 21:53:23 +0100

    Add project files.

---
+++ Single Responsibility Principle/Program.cs
@@ -0,0 +1,57 @@
+﻿
+using System.Diagnostics;
+using static System.Console;
+
+namespace DesignPatterns
+{
+
+    public class Journal
+    {
+        private readonly List<string> entries = new List<string>();
+        private static int count = 0;
+
+        public int AddEntry(string text)
+        {
+            entries.Add($"{++count}: {text}");
+            return count; // memento
+        }
+
+        public void RemoveEntry(int index)
+        {
+            entries.RemoveAt(index);
+        }
+
+
+        public override string ToString()
+        {
+            return string.Join(Environment.NewLine, entries);
+        }
+    }
+
+    public class Persistence
+    {
+        public void SaveToFile(Journal journal, string filename, bool overwrite = false)
+        {
+            if (overwrite || !File.Exists(filename))
+            {
+                File.WriteAllText(filename, journal.ToString());
+            }
+        }
+    }
+
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var j = new Journal();
+            j.AddEntry("I cried today.");
+            j.AddEntry("I ate a bug.");
+            WriteLine(j);
+
+            var p = new Persistence();
+            var filename = @"C:\temp\journal.txt";
+            p.SaveToFile(j, filename, true);
+            Process.Start(new ProcessStartInfo("notepad.exe", filename));
+        }
+    }
+}
