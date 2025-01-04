ee2875ef35fb1470df6f06c99750d6efa13d0d77
Krzysztof Wozniczak <kwoznicz@volvocars.com>
2025-01-04 13:14:54 +0100

    Life Without Builder

---
+++ Life Without Builder/Program.cs
@@ -0,0 +1,29 @@
+﻿using static System.Console;
+
+namespace DesignPatterns
+{
+      
+    public class Program
+    {
+
+        public static void Main(string[] args)
+        {
+            var hello = "Hello";
+            var sb = new System.Text.StringBuilder();
+            sb.Append("<p>");
+            sb.Append(hello);
+            sb.Append("</p>");
+            WriteLine(sb);
+
+            var words = new[] { "Hello", "world" };
+            sb.Clear();
+            sb.Append("<ul>");
+            foreach (var word in words)
+            {
+                sb.Append($"<li>{word}</li>");
+            }
+            sb.Append("</ul>");
+            WriteLine(sb);
+        }
+    }
+}
